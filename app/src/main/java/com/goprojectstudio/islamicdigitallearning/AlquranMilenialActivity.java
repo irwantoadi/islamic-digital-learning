bc131ff29797068f94783e84cd2dbb898631c185
adiirwanto <irwantoadi926@gmail.com>
2020-10-19 11:44:30 +0700

    perbaikan tampilan

--- app/src/main/java/com/goprojectstudio/islamicdigitallearning/AlquranMilenialActivity.java
+++ app/src/main/java/com/goprojectstudio/islamicdigitallearning/AlquranMilenialActivity.java
@@ -2,13 +2,26 @@ package com.goprojectstudio.islamicdigitallearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AlquranMilenialActivity extends AppCompatActivity {
 
+    ImageButton btn_tanya_ustad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alquran_milenial);
+
+        btn_tanya_ustad = findViewById(R.id.btn_tanya);
+        btn_tanya_ustad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
